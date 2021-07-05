7ebc71f14e6d6c9ea4649da298b9e5f0a4cc64a7
Richie78321 <Richie78321@users.noreply.github.com>
2021-07-05 19:30:09 +0000

    Apply Prettier formatting changes

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
 export default function Home({ branches }) {
   return (
     <div className="container text-center">
-      <img src="/branches-logo.svg" className="img-fluid" alt="Pitt CSC Branches" />
-        <ul className="list-group">
+      <img
+        src="/branches-logo.svg"
+        className="img-fluid"
+        alt="Pitt CSC Branches"
+      />
+      <ul className="list-group">
         {branches.map((branch) => (
-          <a href={branch.url} className="list-group-item list-group-item-action">
+          <a
+            href={branch.url}
+            className="list-group-item list-group-item-action"
+          >
             {branch.name}
             {branch.description ? `: ${branch.description}` : null}
           </a>
@@ -17,10 +24,14 @@ export default function Home({ branches }) {
 }
 
 export async function getStaticProps() {
-  let branchesRepos = await (await fetch('https://api.github.com/orgs/pittcsc-branches/repos')).json();
+  let branchesRepos = await (
+    await fetch("https://api.github.com/orgs/pittcsc-branches/repos")
+  ).json();
 
   // Filter out the homepage from the list.
-  branchesRepos = branchesRepos.filter((branchesRepo) => !branchesRepo.name.includes(".github.io"));
+  branchesRepos = branchesRepos.filter(
+    (branchesRepo) => !branchesRepo.name.includes(".github.io")
+  );
 
   return {
     props: {
