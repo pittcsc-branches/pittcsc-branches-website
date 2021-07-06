793010011426f5e2f98df169e5b568b26efef5ba
GameDog9988 <alex@grattan.me>
2021-07-05 20:36:12 -0400

    feat: discord link

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
 import fetch from "node-fetch";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDiscord } from "@fortawesome/free-brands-svg-icons";
 
 export default function Home({ branches }) {
   return (
-    <main className="container mx-auto pb-24 px-4">
+    <main className="container mx-auto pb-16 px-4">
       <img
         src="/branches-logo.svg"
-        className="mx-auto max-w-3xl max-h-96"
+        className="mx-auto max-w-2xl"
         alt="Pitt CSC Branches"
       />
-      <div className="flex flex-col items-center justify-center space-y-8 lg:flex-row lg:gap-8 lg:space-y-0">
-        <div className="bg-secondary-200 mx-auto p-8 max-w-xl leading-loose rounded-lg">
-          <h2 className="mb-4 text-2xl font-bold lg:text-4xl">
+      <div className="flex flex-col justify-center space-y-8 lg:flex-row lg:gap-8 lg:space-y-0">
+        <div className="bg-secondary-200 mx-auto p-8 max-w-2xl leading-loose rounded-lg shadow-md">
+          <h2 className="mb-4 text-2xl font-bold md:text-3xl lg:text-4xl">
             What are branches?{" "}
             <span className="inline-block transform hover:-rotate-6 hover:scale-110 transition">
               🤔
@@ -24,20 +26,19 @@ export default function Home({ branches }) {
           to focus less on monotonous paperwork and slow processes, and more on
           what matters - their community, passions, missions, and members.
         </div>
-        <div className="mx-auto p-8 max-w-xl leading-loose bg-gray-100 rounded-lg space-y-2">
-          <h2 className="mb-4 text-2xl font-bold lg:text-4xl">
+        <div className="a grid justify-items-start place-content-center mx-auto p-8 max-w-xl leading-loose bg-gray-100 rounded-lg shadow-md space-y-2">
+          <h2 className="mb-4 text-2xl font-bold md:text-3xl lg:text-4xl">
             Links to sites{" "}
             <span className="inline-block transform hover:rotate-6 hover:scale-110 transition">
               🔗
             </span>
           </h2>
           <ul className="flex flex-col items-start justify-center space-y-4">
-            {branches.map((branch) => (
-              <li>
+            {branches.map((branch, i) => (
+              <li key={i}>
                 <a
                   href={branch.url}
                   className="text-primary hover:underline font-bold"
-                  key={branch.name}
                 >
                   {branch.name}
                 </a>
@@ -46,6 +47,24 @@ export default function Home({ branches }) {
             ))}
           </ul>
         </div>
+      </div>
+      <div className="flex items-center justify-center mt-16 text-center space-x-2">
+        <p>
+          Join our{" "}
+          <a
+            href="https://discord.gg/wzPeq2GCRT"
+            className="text-primary hover:underline font-bold"
+          >
+            Discord
+          </a>
+          , that's where you can find them!{" "}
+        </p>
+        <a
+          href="https://discord.gg/wzPeq2GCRT"
+          className="text-primary block text-4xl transform hover:scale-105 transition"
+        >
+          <FontAwesomeIcon icon={faDiscord} />
+        </a>
       </div>
     </main>
   );
